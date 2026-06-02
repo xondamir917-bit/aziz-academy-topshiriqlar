2e2a7668ac5087b803c22992909bdc7a01bb8d92
xondamir917-bit <xondamir917@gmail.com>
2026-06-02 08:35:56 +0500

    Add AI task: Kafedra menyusini tuzish

---
+++ ai_topshiriqlar/Kafedra_menyusini_tuzish.py
@@ -0,0 +1,16 @@
+# Kafedra menyusini tuzish
+# Kurs: IT Dasturlash
+# Mavzu: Shartlar bilan menyu tuzish (oddiy CLI menyu)
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+tanlov = int(input())
+
+if tanlov == 1:
+    print("Shashlik narxi 15000 so'm")
+elif tanlov == 2 :
+    print("Shurpa narxi 10000 so'm")
+elif tanlov == 3:
+    print("Manti narxi 12000 so'm")
+else:
+    print("Noto'g'ri tanlov")
