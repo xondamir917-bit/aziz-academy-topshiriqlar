74ba49a4130a5a025d72827cd8bdb7ed9c30f522
xondamir917-bit <xondamir917@gmail.com>
2026-06-02 08:51:33 +0500

    Add AI task: Kafedra uchun dars jadvali

---
+++ ai_topshiriqlar/Kafedra_uchun_dars_jadvali.py
@@ -0,0 +1,14 @@
+# Kafedra uchun dars jadvali
+# Kurs: IT Dasturlash
+# Mavzu: 1-mavzu: Python va dasturlashga kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+n = int(input())
+for i in range(n):
+    malumot = input().split()
+    boshlash_vaqti = malumot[-2]
+    tugash_vaqti = malumot[-1]
+    dars_nomi = " ".join(malumot[:-2])
+    
+    print(f"{dars_nomi} {boshlash_vaqti}-{tugash_vaqti}")
