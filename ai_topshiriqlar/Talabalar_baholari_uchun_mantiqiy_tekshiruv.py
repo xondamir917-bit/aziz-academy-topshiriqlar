e446e219e4ae9c975cf0e9a401e1535ca97da101
xondamir917-bit <xondamir917@gmail.com>
2026-06-03 09:09:42 +0500

    Add AI task: Talabalar baholari uchun mantiqiy tekshiruv

---
+++ ai_topshiriqlar/Talabalar_baholari_uchun_mantiqiy_tekshiruv.py
@@ -0,0 +1,13 @@
+# Talabalar baholari uchun mantiqiy tekshiruv
+# Kurs: IT Dasturlash
+# Mavzu: Mantiqiy operatorlar: and, or, not
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+baho = int(input())
+ishtirok_foizi = int(input())
+
+if baho > 50 and ishtirok_foizi > 75:
+    print("Imtihondan o'tdi")
+else:
+    print("Imtihondan o'tmadi")
