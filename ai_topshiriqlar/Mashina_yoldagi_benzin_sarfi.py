4a4467de21d6b335059a632a003a4b70ed93059b
xondamir917-bit <xondamir917@gmail.com>
2026-06-04 09:20:41 +0500

    Add AI task: Mashina yo'ldagi benzin sarfi

---
+++ ai_topshiriqlar/Mashina_yoldagi_benzin_sarfi.py
@@ -0,0 +1,13 @@
+# Mashina yo'ldagi benzin sarfi
+# Kurs: IT Dasturlash
+# Mavzu: Ma’lumot turlari: int, float
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+km, m = map(int, input().split())
+max_km = m * 10 
+
+if max_km >= km:
+    print(float(km))
+else:
+    print(float(max_km))
