ca6c0c891ca93a0a676849a6fcd5cca94727d0c4
xondamir917-bit <xondamir917@gmail.com>
2026-06-04 09:08:56 +0500

    Add AI task: Kino chiptasi narxi

---
+++ ai_topshiriqlar/Kino_chiptasi_narxi.py
@@ -0,0 +1,15 @@
+# Kino chiptasi narxi
+# Kurs: IT Dasturlash
+# Mavzu: Ichma-ich shartlar (nested if)
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+age = int(input())
+if age < 18:
+    print("5000")
+elif age > 18 and age < 60:
+    print("8000")
+elif age > 60:
+    print("5000")
+else:
+    print("5000")
