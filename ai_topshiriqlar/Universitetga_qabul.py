67a943751edfb3d2b2a0a5e6521bde86386da4d7
xondamir917-bit <xondamir917@gmail.com>
2026-06-04 09:15:23 +0500

    Add AI task: Universitetga qabul

---
+++ ai_topshiriqlar/Universitetga_qabul.py
@@ -0,0 +1,14 @@
+# Universitetga qabul
+# Kurs: IT Dasturlash
+# Mavzu: Ichma-ich shartlar (nested if)
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+y, b = map(int, input().split())
+if y > 20:
+    if b > 50:
+        print("Talaba universitetga qabul qilindi")
+    else:
+        print("Talaba qabulga layoqatsiz")
+else:
+    print("Talaba qabulga layoqatsiz")
