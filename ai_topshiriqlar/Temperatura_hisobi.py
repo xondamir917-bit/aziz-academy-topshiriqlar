acf3351eb278b5b64289e2078c2ec037faa7a584
xondamir917-bit <xondamir917@gmail.com>
2026-06-05 13:36:25 +0500

    Add AI task: Temperatura hisobi

---
+++ ai_topshiriqlar/Temperatura_hisobi.py
@@ -0,0 +1,9 @@
+# Temperatura hisobi
+# Kurs: IT Dasturlash
+# Mavzu: Ma’lumot turlari: int, float
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+a, b = map(int, input().split())
+f = (a + b) / 2 
+print(f)
