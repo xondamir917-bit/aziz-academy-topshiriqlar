13992c12d2ca4bfcb242c376f4f77ce9bbd0503c
xondamir-dev <xondamir917@gmail.com>
2026-06-07 12:04:01 +0500

    Add AI task: Oʻquvchi baholarini tekshirish dasturi

---
+++ ai_topshiriqlar/Oʻquvchi_baholarini_tekshirish_dasturi.py
@@ -0,0 +1,19 @@
+# Oʻquvchi baholarini tekshirish dasturi
+# Kurs: IT Dasturlash
+# Mavzu: if, elif, else asoslari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+oquvchi = input().split()
+baho = int(oquvchi[1])
+ism = str(oquvchi[0])
+if baho > 90:
+    print(f"{ism} A")
+elif baho > 80 and baho < 89:
+    print(f"{ism} B")
+elif baho > 70 and baho < 79:
+    print(f"{ism} C")
+elif baho > 60 and baho < 69:
+    print(f"{ism} D")
+else:
+    print(f"{ism} F")
