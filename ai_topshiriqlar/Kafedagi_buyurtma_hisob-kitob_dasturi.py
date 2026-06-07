498adc5ba12bb40de5f257fa83536dd326e3c2a4
xondamir-dev <xondamir917@gmail.com>
2026-06-07 12:29:21 +0500

    Add AI task: Kafedagi buyurtma hisob-kitob dasturi

---
+++ ai_topshiriqlar/Kafedagi_buyurtma_hisob-kitob_dasturi.py
@@ -0,0 +1,11 @@
+# Kafedagi buyurtma hisob-kitob dasturi
+# Kurs: IT Dasturlash
+# Mavzu: O‘zgaruvchilar va nomlash qoidalari
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+b = float(input())
+cm = float(input())
+s = float(input())
+j = b * (1 - cm / 100)
+print(j)
