57b9666bc870315521202b3fb2d5bb7e62410ff9
xondamir-dev <xondamir917@gmail.com>
2026-06-07 12:11:43 +0500

    Add AI task: Valyuta kursi hisobi

---
+++ ai_topshiriqlar/Valyuta_kursi_hisobi.py
@@ -0,0 +1,12 @@
+# Valyuta kursi hisobi
+# Kurs: IT Dasturlash
+# Mavzu: Ma’lumot turlari: int, float
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+dollar = int(input())
+x = float(input())
+y = float(input())
+
+print(float(f"{x * dollar}"))
+print(float(f"{y * dollar}"))
