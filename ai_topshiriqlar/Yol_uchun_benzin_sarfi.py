e72adf6a10058daf0a96479a3d94f9c87443c05a
xondamir-dev <xondamir917@gmail.com>
2026-06-08 11:59:48 +0500

    Add AI task: Yo'l uchun benzin sarfi

---
+++ ai_topshiriqlar/Yol_uchun_benzin_sarfi.py
@@ -0,0 +1,9 @@
+# Yo'l uchun benzin sarfi
+# Kurs: IT Dasturlash
+# Mavzu: Ma’lumot turlari: int, float
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+masofa = float(input())
+_ = float(input())
+print(masofa / 12)
