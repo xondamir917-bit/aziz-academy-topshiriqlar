32865212315e810855736414907a7d242bf77c0c
xondamir-dev <xondamir917@gmail.com>
2026-06-08 11:18:54 +0500

    Add: 15) Menyu: yakuniy [xondamir917-bit]

---
+++ lms_kod_topshiriqlari/Shartlar_bilan_menyu_tuzish__oddiy_CLI_menyu_/15__Menyu__yakuniy.py
@@ -0,0 +1,10 @@
+
+choice = input()
+if choice == "start":
+    print("Started")
+elif choice == "stop":
+    print("Stopped")
+elif choice == "pause":
+    print("Paused")
+else:
+    print("Unknown command")
