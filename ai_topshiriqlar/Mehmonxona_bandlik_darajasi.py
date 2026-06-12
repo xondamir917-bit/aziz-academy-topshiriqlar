89810b91540f7bb06618a5cc603f4fa0d54f5f5a
xondamir-dev <xondamir917@gmail.com>
2026-06-12 14:00:44 +0500

    Add AI task: Mehmonxona bandlik darajasi

---
+++ ai_topshiriqlar/Mehmonxona_bandlik_darajasi.py
@@ -0,0 +1,16 @@
+# Mehmonxona bandlik darajasi
+# Kurs: Dasturlash / IT
+# Mavzu: Ma’lumot turlari: bool va mantiqiy ifodalar
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+# starter Python code
+u = int(input())
+b = int(input())
+ba = b / u * 100 
+
+print(ba)
+if ba > 50:
+    print("Mehmonxona ko'p band")
+else:
+    print("Mehmonxona ozgina band")
