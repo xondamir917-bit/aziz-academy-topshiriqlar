4db7fedf985de33e74f75b650755ba51f3c438bc
xondamir-dev <xondamir917@gmail.com>
2026-06-13 11:43:09 +0500

    Add AI task: Matnni qisqartirish

---
+++ ai_topshiriqlar/Matnni_qisqartirish.py
@@ -0,0 +1,14 @@
+# Matnni qisqartirish
+# Kurs: Dasturlash / IT
+# Mavzu: Ma’lumot turlari: str (string) kirish
+# Ball: 100
+# Aziz Academy — AI Topshiriq
+
+matn = input()
+n = int(input())
+
+if len(matn) <= n:
+    print(matn)
+else:
+    s = matn.split()
+    print(" ".join(s[:-1]))
